3da81dcf4bb4edf14ee1277a601d767eb884506b
e1b19109 <e1b19109@st.oit.ac.jp>
2021-10-30 19:26:28 +0900

    弾数制限
    初期持ち弾数は10
    弾薬アイテムを取ることで＋５回復する

--- Assets/SampleShooting/RocketController.cs
+++ Assets/SampleShooting/RocketController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class RocketController : MonoBehaviour {
 
 	public GameObject bulletPrefab;
+    int i = 10;
 
 	void Update () {
 		if (Input.GetKey (KeyCode.LeftArrow)&transform.position.x > -9.0f) {
@@ -13,10 +14,18 @@ public class RocketController : MonoBehaviour {
 		if (Input.GetKey (KeyCode.RightArrow)&transform.position.x < 9.0f) {
 			transform.Translate ( 0.1f, 0, 0);
 		}
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			Instantiate (bulletPrefab, transform.position, Quaternion.identity);
-		}
-	}
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (i > 0)
+            {
+                Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+                i--;
+            }
+            else {
+                i = 0;
+            }
+        }
+    }
 
     void OnTriggerEnter2D(Collider2D coll)
     {
@@ -28,8 +37,8 @@ public class RocketController : MonoBehaviour {
         }
 	if (coll.gameObject.tag == "item")
         {
+            i += 5;
             Destroy(coll.gameObject);
-            
         }
     }
 }
