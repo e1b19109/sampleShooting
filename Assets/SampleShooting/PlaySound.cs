09ea40d86762e86de99b07c4093cdeec5bd22e83
sige1211 <e1b19065@oit.ac.jp>
2021-10-31 17:02:37 +0900

    音の調整＆GameOver画面追加
    。。。

--- Assets/SampleShooting/PlaySound.cs
+++ Assets/SampleShooting/PlaySound.cs
@@ -4,16 +4,28 @@ using System.Collections;
 public class PlaySound : MonoBehaviour {
 		
 	private AudioSource sound01;
+	GameObject rocket1;
+	RocketController rocket2;
+
 
 	void Start () {
 		//AudioSourceコンポーネントを取得し、変数に格納
 		sound01 = GetComponent<AudioSource>();
+		//RocketControllerの弾数を取得、格納
+		rocket1 = GameObject.Find("rocket");
+		Debug.Log(rocket1);
+		rocket2 = rocket1.GetComponent<RocketController>();
 	}
 	
 	void Update () {
 		//指定のキーが押されたら音声ファイル再生
-		if(Input.GetKeyDown(KeyCode.Space)) {
-			sound01.PlayOneShot(sound01.clip);
+		if (rocket2.i > 0)
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				sound01.PlayOneShot(sound01.clip);
+			}
 		}
+
 	}
 }
