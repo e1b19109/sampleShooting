bb9f237f4685ecf15703ad1f55107ffa7cf690e6
sige1211 <e1b19065@oit.ac.jp>
2021-10-31 21:36:57 +0900

    add_noting2
    弾を落ちなくしました。

--- Assets/SampleShooting/itemGenerator.cs
+++ Assets/SampleShooting/itemGenerator.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class itemGenerator : MonoBehaviour
 {
     public GameObject itemPrefab;
+    GameObject canvas;
+    UIController gameFinish1;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("GenRock", 1, 10);
+        canvas = GameObject.Find("Canvas");
+        Debug.Log(canvas);
+        gameFinish1 = canvas.GetComponent<UIController>();
     }
 
     // Update is called once per frame
     void GenRock()
     {
+        if (gameFinish1.gameFinish == true)
+        {
+            return;
+        }
         Instantiate(itemPrefab, new Vector3 (-9f + 16 * Random.value, 6, 0), Quaternion.identity);
     }
 }
